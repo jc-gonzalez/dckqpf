8fe13e298343769ed773811f69fb8d74119ce1d6
J C Gonzalez <JCGonzalez@sciops.esa.int>
2018-09-17 10:02:57 +0200

    Minor upgrade of start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,8 +2,9 @@
 source env_qpf.sh
 qpfexe=$(which qpf)
 ip=$(hostname -i)
-sed -e "s|@QPF_MASTER_IP@|$ip|g" /home/eucops/qpf/cfg/qpf-test.tpl.cfg \
-    > /home/eucops/qpf/cfg/qpf-test.cfg
-${qpfexe} -v -v -v -v -c /home/eucops/qpf/cfg/qpf-test.cfg -I ${ip} 2>&1 \
-    | tee ${HOME}/qpf/data/QPF.log
+sudo chown -R ${USER_NAME}.${GRP_NAME} /home/${USER_NAME}/qpf/
+sed -e "s|@QPF_MASTER_IP@|$ip|g" /home/${USER_NAME}/qpf/cfg/qpf-test.tpl.cfg \
+    > /home/${USER_NAME}/qpf/cfg/qpf-test.cfg
+${qpfexe} -v -v -v -v -c /home/${USER_NAME}/qpf/cfg/qpf-test.cfg -I ${ip} 2>&1 \
+    | tee /home/${USER_NAME}/qpf/data/QPF.log
 
